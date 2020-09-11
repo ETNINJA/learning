9bc5f7cd79dbace6304155203c5eddadd6ff68ea
ETNinja <annealmd@gmail.com>
2020-09-11 11:15:29 -0300

    Theme File and debugBanner false

---
+++ app_1/lib/myTheme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final ThemeData myTheme = ThemeData(
+  //primarySwatch: MaterialColor(primary, swatch),
+  primaryColor: Colors.deepPurple[400],
+);
---
+++ app_1/lib/screens/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First App'),
+      ),
+      body: Center(
+        child: Text(' trying First App'),
+      ),
+    );
+  }
+}
