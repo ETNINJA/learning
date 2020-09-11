c0e42318456d7916ec99d1ee325e87d3322bc2be
ETNinja <annealmd@gmail.com>
2020-09-11 15:56:27 -0300

    RaisedButton: pointer function and annonymus function and big function

--- app_1/lib/screens/homepage.dart
+++ app_1/lib/screens/homepage.dart
@@ -25,11 +25,14 @@ class Homepage extends StatelessWidget {
                 color: Colors.green[300]),
             RaisedButton(
                 child: Text('Answer2'),
-                onPressed: answerQuestion,
+                onPressed: () => print('Question 2!'),
                 color: Colors.pink[300]),
             RaisedButton(
                 child: Text('Answer3'),
-                onPressed: answerQuestion,
+                onPressed: () {
+                  // lots of actions
+                  print('Answer 3');
+                },
                 color: Colors.blue[300]),
           ],
         ),
