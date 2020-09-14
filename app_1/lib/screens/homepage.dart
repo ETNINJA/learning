b5bedf4c3b9e6b69dbcefd52c9912838804d0e66
ETNinja <annealmd@gmail.com>
2020-09-14 15:05:16 -0300

    if statement in the function _answerQuestion

--- app_1/lib/screens/homepage.dart
+++ app_1/lib/screens/homepage.dart
@@ -12,37 +12,40 @@ class _HomepageState extends State<Homepage> {
   var _questionIndex = 0;
   var _index = 0;
 
+  final questions = [
+    // would set it as const but colors does not allow.
+    // now it's a list of Maps'
+    {
+      'questionText': 'What\'s your favourite Colour?',
+      'answers': ['Black', 'Pink', 'Blue'],
+      'buttonColor': Colors.green[300],
+    },
+    {
+      'questionText': 'What\'s  your favourite animal?',
+      'answers': ['Cat', 'Dog', 'Elephant'],
+      'buttonColor': Colors.pink[300],
+    },
+    {
+      'questionText': 'What\'s  your favourite sweet?',
+      'answers': ['Brigadeiro', 'Chocolate', 'Sorvete'],
+      'buttonColor': Colors.blue[300],
+    },
+  ];
+
   void _answerQuestion() {
-    print('Answer Chosen!');
-    setState(() {
-      _questionIndex++;
-      _index = 0;
-    });
+    _questionIndex++;
+    if (_questionIndex < questions.length) {
+      print('Answer Chosen!');
+      setState(() {
+        _index = 0;
+      });
+    }
 
     print('INDEX  = $_questionIndex');
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      // would set it as const but colors does not allow.
-      // now it's a list of Maps'
-      {
-        'questionText': 'What\'s your favourite Colour?',
-        'answers': ['Black', 'Pink', 'Blue'],
-        'buttonColor': Colors.green[300],
-      },
-      {
-        'questionText': 'What\'s  your favourite animal?',
-        'answers': ['Cat', 'Dog', 'Elephant'],
-        'buttonColor': Colors.pink[300],
-      },
-      {
-        'questionText': 'What\'s  your favourite sweet?',
-        'answers': ['Brigadeiro', 'Chocolate', 'Sorvete'],
-        'buttonColor': Colors.blue[300],
-      },
-    ];
     return Scaffold(
       appBar: AppBar(
         title: Text('Project One'),
