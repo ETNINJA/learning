9debf027011d1b0ca25ea2591fe3e18277b36d50
EtNinja <annealmd@gmail.com>
2020-09-16 15:28:10 -0300

    init new project

---
+++ app_2/lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final ThemeData myTheme = ThemeData(
+  //primarySwatch: MaterialColor(primary, swatch),
+  primaryColor: Color(0xffdcb9ca),
+  buttonColor: Color(0xff3CAEA3),
+);
