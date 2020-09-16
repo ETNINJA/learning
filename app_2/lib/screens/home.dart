2a708cbecb050d498233dd11e063b932867b62d6
EtNinja <annealmd@gmail.com>
2020-09-16 16:34:12 -0300

    frame for the 2 parts screen chart tx and list tx

--- app_2/lib/screens/home.dart
+++ app_2/lib/screens/home.dart
@@ -4,8 +4,27 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('You are In!'),
-    ));
+        appBar: AppBar(
+          title: Text('Expenses Planner'),
+        ),
+        body: Column(
+          children: [
+            Card(
+              child: Text(
+                'chart of the daily transactions',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            Divider(
+              height: 5,
+            ),
+            Card(
+              child: Text(
+                'List of transactions',
+                style: TextStyle(fontSize: 24),
+              ),
+            )
+          ],
+        ));
   }
 }
