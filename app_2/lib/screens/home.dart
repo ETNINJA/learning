f18eaa11d107e3e6c2de2b9db04cdddcb6d36ba3
EtNinja <annealmd@gmail.com>
2020-09-18 16:40:49 -0300

    SingleChildScrollView, avoiding overflow

--- app_2/lib/screens/home.dart
+++ app_2/lib/screens/home.dart
@@ -14,33 +14,35 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Expenses Planner'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Card(
-            color: Color(0xFFF1F7FA),
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20)),
-                side: BorderSide(
-                    width: 3, color: Theme.of(context).primaryColor)),
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: Text(
-                'chart of the daily transactions',
-                style: TextStyle(fontSize: 20),
-                textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              color: Color(0xFFF1F7FA),
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
+                  side: BorderSide(
+                      width: 3, color: Theme.of(context).primaryColor)),
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: Text(
+                  'chart of the daily transactions',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
-          ),
-          UserTransactions(),
-          //user_transactions will manage the inputs and list
-          // its a Statefull Widget
-        ],
+            UserTransactions(),
+            //user_transactions will manage the inputs and list
+            // its a Statefull Widget
+          ],
+        ),
       ),
     );
   }
